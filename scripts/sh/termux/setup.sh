41a4c1832c7d4e7b825aa635e5beb25408057872
Mbilse <mbilse001@outlook.com>
2025-03-03 20:58:59 +0800

    Create setup.sh

---
+++ scripts/sh/termux/setup.sh
@@ -0,0 +1,12 @@
+termux-setup-storage
+pkg install proot
+termux-chroot
+apt install cmatrix
+apt install git
+apt install o-editor
+apt install kona
+apt install tree
+apt install c-script
+apt install yazi
+apt install vim
+apt install nodejs
