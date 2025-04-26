25deaa773230f418cc35c54ff47621238ad266af
Mbilse <mbilse001@outlook.com>
2025-04-26 11:02:15 +0800

    Add files via upload

---
+++ nbnhhsh/nbnhhsh.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         能不能好好说话？
+// @namespace    https://lab.magiconch.com/nbnhhsh
+// @version      0.15
+// @description  首字母缩写划词翻译工具
+// @author       itorr
+// @license      MIT
+// @icon         https://lab.magiconch.com/favicon.ico
+// @match        *://weibo.com/*
+// @match        *://*.weibo.com/*
+// @match        *://*.weibo.cn/*
+// @match        *://tieba.baidu.com/*
+// @match        *://*.bilibili.com/
+// @match        *://*.bilibili.com/*
+// @match        *://*.douban.com/group/*
+// @require      https://lab.magiconch.com/vue.2.6.11.min.js
+// @inject-into  content
+// @grant        none
+// ==/UserScript==
