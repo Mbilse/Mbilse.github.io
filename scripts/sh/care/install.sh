0dff20bee098c3d2520eac49cfe01146c6db334e
Mbilse <mbilse001@outlook.com>
2025-05-10 18:16:45 +0800

    Create install.sh

---
+++ scripts/sh/care/install.sh
@@ -0,0 +1,17 @@
+echo "Welcome! 
+"
+sleep 1
+echo "Install MbilseCare 4.0.3"
+echo "2.5MB /0B loading..."
+echo "
+"
+sleep 1
+echo "
+installing..."
+sleep 0.4
+printf "#"
+sleep 1.3
+printf "#"
+sleep 1.6
+printf "#"
+
