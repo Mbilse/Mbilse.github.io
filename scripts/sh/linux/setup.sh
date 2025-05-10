92bf9aec986a3b01f1b4242bb4b139cb61ecfb3b
Mbilse <mbilse001@outlook.com>
2025-05-10 11:49:01 +0800

    Create setup.sh

---
+++ scripts/sh/linux/setup.sh
@@ -0,0 +1,16 @@
+
+apt install git
+git config --global user.name "mbilse"
+git config --global user.email "mbilse001@outlook.com"
+git init
+git --version
+apt install o-editor
+apt install kona
+apt install tree
+apt install c-script
+apt install yazi
+apt install vim
+apt install nodejs
+node -v
+npm -v
+npm install typescript
