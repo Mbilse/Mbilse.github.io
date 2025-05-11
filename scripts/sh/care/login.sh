ade4b9e2e08abbe145dce52a3c2a4fb202b2abfe
Mbilse <mbilse001@outlook.com>
2025-05-11 08:13:34 +0800

    Update login.sh

--- scripts/sh/care/login.sh
+++ scripts/sh/care/login.sh
@@ -8,17 +8,9 @@ echo " "
 sleep 0.5
 printf "Username:"
 read username
-case $username in
-Mbilse)
-;;
-esac
 sleep 7
 printf "Password:"
 read password
-case $password in
-Mbilse)
-;;
-esac
 sleep 7
 touch userdata.txt
 echo "Username:$username
