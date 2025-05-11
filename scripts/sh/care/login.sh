436af4681ac0b2318c31c26d81344ffbdbef62a7
Mbilse <mbilse001@outlook.com>
2025-05-11 08:05:37 +0800

    Update login.sh

--- scripts/sh/care/login.sh
+++ scripts/sh/care/login.sh
@@ -1,3 +1,18 @@
 cd
 cd ./MbilseCare
 
+mkdir "LoginData" > /dev/null 2>&1
+cd ./LoginData
+# Login
+echo " "
+sleep 0.5
+printf "Username:"
+read username
+sleep 0.5
+printf "Password:"
+read password
+sleep 0.5
+touch userdata.txt
+echo "Username:$username
+Password:$password" > username.txt
+echo "Login Done!"
