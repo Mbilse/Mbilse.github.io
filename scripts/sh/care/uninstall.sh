dab929e28059a43455ac3ef6a0b26b6b2d3c35cd
Mbilse <mbilse001@outlook.com>
2025-06-01 07:42:46 +0800

    Create uninstall.sh

---
+++ scripts/sh/care/uninstall.sh
@@ -0,0 +1,103 @@
+echo ""
+echo "Loading... 
+"
+sleep 1
+echo "Install MbilseCare 4.0.3"
+echo "2.5MB /0B loading..."
+echo "
+"
+sleep 1
+echo "
+Waiting for headers..."
+sleep 1.36
+printf "progress:"
+sleep 0.1
+printf "#"
+sleep 1
+printf "#"
+sleep 0.6
+printf "#"
+sleep 0.5
+printf "#"
+sleep 0.4
+printf "#"
+sleep 1
+printf "#"
+sleep 0.5
+printf "#"
+sleep 0.8
+printf "#"
+sleep 0.5
+printf "#"
+sleep 1.4
+printf "#"
+
+echo ""
+sleep 0.5
+echo "
+installing..."
+sleep 0.3
+printf "progress:"
+sleep 0.4
+printf "#"
+cd
+sleep 1.3
+printf "#"
+sleep 1.6
+mkdir "MbilseCare" > /dev/null 2>&1
+printf "#"
+cd ./MbilseCare
+touch config.txt
+sleep 0.7
+echo "awim.kernel=\"mbilsecare\"
+awim.king=\"awimer\"
+api.keys=\"none\"
+api.name=\"none.none.com\"
+app.build=\"001\"
+app.install=\"grand\"
+bin.keysoft=\"keysoft\"
+bin.keys=\"000001\"
+board.id=\"455570\"
+board.rel=\"Mbilse\"
+clip.start=\"new\"
+clip.setup=\"author\"
+clip.storebin=\"yes\"
+clip.install=\"installed\"
+classic.pin=\"245548\"
+classic.project=\"new\"
+dele.run=\"done\"
+http.lan=\"unknown\"
+http.ur=\"https://mbilse.github.io\"
+http.porxy=\"00168\"
+http.port=\"8080\"
+project.name=\"myproject\"
+project.id=\"000001\"
+system.project=\"new\"
+system.version=\"12.9.0\"
+system.osname=\"MbilseOS\"
+system.ostype=\"64bit\"
+user.name=\"Mbilse\"
+user.email=\"mbilse001@outlook.com\"
+" > config.txt
+printf "#"
+touch list.txt
+echo "<UNKNOWN>" > list.txt
+sleep 2.1
+printf "#"
+touch init.txt
+echo "<UNKNOWN>" > init.txt
+
+sleep 1.8
+printf "#"
+sleep 1.2
+printf "#"
+sleep 2.3
+printf "#"
+sleep 1.5
+printf "#"
+sleep 1.7
+printf "#"
+
+echo ""
+sleep 0.2
+echo "MbilseCare (4.0.3) 2.5MB uninstall done!"
