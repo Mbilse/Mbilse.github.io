547c30bafd4a7d97803823bf69ba63f3d47e4419
Mbilse <mbilse001@outlook.com>
2025-06-01 15:22:38 +0800

    Create installapps.sh

---
+++ scripts/sh/care/installapps.sh
@@ -0,0 +1,71 @@
+echo ""
+echo "Loading... 
+"
+sleep 1
+echo "Install MbilseMicroApps 5.0.2"
+echo "1.4KB /0B loading..."
+echo "
+"
+sleep 1
+echo "
+Waiting for headers..."
+sleep 1.36
+printf "progress:"
+sleep 0.1
+printf "#"
+sleep 1
+printf "#"
+sleep 0.6
+printf "#"
+sleep 0.5
+printf "#"
+sleep 0.4
+printf "#"
+sleep 1
+printf "#"
+sleep 0.5
+printf "#"
+sleep 0.8
+printf "#"
+sleep 0.5
+printf "#"
+sleep 1.4
+printf "#"
+
+echo ""
+sleep 0.5
+echo "
+installing..."
+sleep 0.3
+printf "progress:"
+sleep 0.4
+printf "#"
+cd
+sleep 1.3
+printf "#"
+sleep 1.6
+mkdir "MbilseCare" > /dev/null 2>&1
+printf "#"
+cd ./MbilseCare
+sleep 0.7
+printf "#"
+echo "Mbilse MicroAppsInstaller" >> list.txt
+sleep 2.1
+printf "#"
+mkdir "Apps" > /dev/null 2>&1
+cd ./Apps
+mkdir "ID" > /dev/null 2>&1
+sleep 1.2
+printf "#"
+sleep 1.4
+printf "#"
+sleep 1.3
+printf "#"
+sleep 1.4
+printf "#"
+sleep 1.2
+printf "#"
+
+echo ""
+sleep 0.2
+echo "MbilseMicroApps (5.0.2) 1.4KB install done!"
